a6fae69e97ae66fc8486713a400d3d9840ca4f9f
Taro Ohsugi <root@taroosg.dev>
2024-07-10 13:20:36 +0000

    codespace対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    //
+    if (config('app.env') === 'codespaces') {
+      URL::forceRootUrl(config('app.url'));
+      if (str_contains(config('app.url'), 'https://')) {
+        URL::forceScheme('https');
+      }
+    }
   }
 }
